cb406d917e18bd6e50e12ff558116cad2655107f
beachbobe <bobchestnut28@gmail.com>
2024-11-26 17:11:51 -0500

    Added comment to prevent Gtk Gdk missing module messages

--- gridNetwork.py
+++ gridNetwork.py
@@ -4,7 +4,7 @@
 import gi, os, socket, requests
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk
+from gi.repository import Gtk, Gdk # type: ignore  (this supresses warning message)
 
 class SimpleApp(Gtk.Window):
     def __init__(self):
